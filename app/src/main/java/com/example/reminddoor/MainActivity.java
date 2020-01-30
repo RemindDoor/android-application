eabf36a256417d92b126bfadc43b36727923a638
markjswan <markswan98@gmail.com>
2020-01-30 21:19:45 +0000

    trying to change onClick response for bottom navigation bar

--- app/src/main/java/com/example/reminddoor/MainActivity.java
+++ app/src/main/java/com/example/reminddoor/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.reminddoor;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.reminddoor.ui.dashboard.DashboardFragment;
+import com.example.reminddoor.ui.home.HomeFragment;
+import com.example.reminddoor.ui.notifications.RemindersFragment;
 import com.example.reminddoor.ui.notifications.list.RemindersCalendarContainer;
+import com.example.reminddoor.ui.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +27,8 @@ import java.io.ObjectOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+	BottomNavigationView bottomNavigationView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,16 +36,46 @@ public class MainActivity extends AppCompatActivity {
 		
 		setContentView(R.layout.activity_main);
 		BottomNavigationView navView = findViewById(R.id.nav_view);
+		navView.setOnNavigationItemSelectedListener(navListener);
 		// Passing each menu ID as a set of Ids because each
 		// menu should be considered as top level destinations.
 		AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-				R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.profile_notifications)
+				R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile)
 				.build();
 		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 		NavigationUI.setupWithNavController(navView, navController);
+
 	}
-	
+
+	private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+			new BottomNavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+					Fragment selectedFragment = null;
+
+					switch(item.getItemId()) {
+						case R.id.navigation_home:
+							selectedFragment = new HomeFragment();
+							break;
+						case R.id.navigation_notifications:
+							selectedFragment = new RemindersFragment();
+							break;
+						case R.id.navigation_dashboard:
+							selectedFragment = new DashboardFragment();
+							break;
+						case R.id.navigation_profile:
+							selectedFragment = new ProfileFragment();
+							break;
+
+					}
+
+					getSupportFragmentManager().beginTransaction().replace(R.id.navigation_home, selectedFragment).commit();
+					return true;
+				}
+			};
+
 	@Override
 	protected void onStop() {
 		super.onStop();
