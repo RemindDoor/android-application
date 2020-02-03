11b5c4d2520ec23ec373f08d87ed03094378389a
Cameron Watt <s1637380@ed.ac.uk>
2020-02-03 13:53:49 +0000

    send basic notification on connection to bluetooth device

--- app/src/main/java/com/example/reminddoor/BluetoothReceiver.java
+++ app/src/main/java/com/example/reminddoor/BluetoothReceiver.java
@@ -1,10 +1,14 @@
 package com.example.reminddoor;
 
+import android.app.Notification;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class BluetoothReceiver extends BroadcastReceiver {
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -18,6 +22,7 @@ public class BluetoothReceiver extends BroadcastReceiver {
                 if (device.getAddress().equals(headphones.getAddress())) {
                     //Do what you want
                     System.out.println("Connected to bluetooth device");
+                    addImportantNotification(context);
                 }
                 break;
             }
@@ -27,5 +32,21 @@ public class BluetoothReceiver extends BroadcastReceiver {
                 break;
             }
         }
+    }
+
+    private void addImportantNotification(Context context){
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        Notification notification2 = new NotificationCompat.Builder(context, NotificationSetup.CHANNEL_1_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("RemindDoor")
+                .setContentText("Door Found- unlock device to open your door")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Unlock device to open your door"))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        notificationManager.notify(0, notification2);
     }
 }
